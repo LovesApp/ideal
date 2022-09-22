6559e4937f1aa5f8d0186073008d6649f9ef4237
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-22 16:47:19 +0200

    Create vib.js

---
+++ src/app/vib.js
@@ -0,0 +1,11 @@
+// The haptic engine pop vibrate
+    CM_APIController.VibrationAsPop();
+
+    // The haptic engine Peek vibrate
+    CM_APIController.VibrationAsPeek();
+
+    // The haptic engine Nope vibrate
+    CM_APIController.VibrationAsNope();
+
+    // Vibrate device in milesecond (Android only)
+    CM_APIController.Vibration(1000);
